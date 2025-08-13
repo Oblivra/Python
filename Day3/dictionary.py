30129b98ddde02d405c8574e08a57a04c2b798ec
Unknownityyy <pprem1644@gmail.com>
2025-08-13 14:44:54 +0000

    dictionary

---
+++ Day3/dictionary.py
@@ -0,0 +1,19 @@
+# 
+# 
+# 
+# Mutable
+
+marks = {
+    "harry" : 100,
+    "shubaham" : 56,
+    "rohan" : 24
+}
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"harry": 99})
+# print(marks)
+print(marks.get("harry"))
+
+print(marks.get("harry2"))   
+print(marks["harry2"])
