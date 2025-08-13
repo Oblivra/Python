00cabf354f0f6b12026d4333184dbf95b22df883
Unknownityyy <pprem1644@gmail.com>
2025-08-13 08:55:06 +0000

    list method

---
+++ Day3/list_methods.py
@@ -0,0 +1,15 @@
+names = ["demon slayer", "solo leveling", "attack on titan", 333.4 , True , False, 
+            2, "spyxfamily" ]
+
+names.append("jujutsu kaisen")
+print(names)
+
+
+numbers = [ 3 , 74, 33, 66, 7, 8,2,45,1]
+# numbers.sort()
+# numbers.reverse()
+# numbers.append(444)
+# numbers.insert(3, 33333333)
+# numbers.pop(2) 
+# numbers.remove(33) 
+print(numbers)
