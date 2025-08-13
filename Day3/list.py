2e1d2d5a02be488eba9d95aaaf4b1cca4da342d6
Unknownityyy <pprem1644@gmail.com>
2025-08-13 08:54:20 +0000

    list

---
+++ Day3/list.py
@@ -0,0 +1,33 @@
+names = ["demon slayer", "solo leveling", "attack on titan", 333.4 , True , False, 
+            2, "spyxfamily" ]
+
+#  List are mutable ( can be changed )
+
+#  [ string are immutable ( can not be changed ) ]
+
+print(names[0])
+
+names[0] = "manga"
+
+print(names[0])
+ 
+print(names[0:5])
+
+fruits = []
+
+f1 = input("Enter your fruits: ")
+fruits.append(f1)
+f2 = input("Enter your fruits: ")
+fruits.append(f2)
+f3 = input("Enter your fruits: ")
+fruits.append(f3)
+f4 = input("Enter your fruits: ")
+fruits.append(f4)
+f5 = input("Enter your fruits: ")
+fruits.append(f5)
+f6 = input("Enter your fruits: ")
+fruits.append(f6)
+f7 = input("Enter your fruits: ")
+fruits.append(f7)
+
+print(fruits)
