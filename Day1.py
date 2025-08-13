7017e923e8ed7d81da24dfb0b19f9814589e1e17
Unknownityyy <pprem1644@gmail.com>
2025-08-13 07:32:55 +0000

    Day1

---
+++ Day1.py
@@ -0,0 +1,26 @@
+# 
+# 
+# 
+# Day 1 and Day 2 deleted by mistake. 
+# I was doing in codespace and i deleted repo for some commit inconsistency 
+# so the codespace was also gone.
+
+# Here what i learned in day1 and day2
+
+# day1
+  -  printing
+  -  hello world
+  -  using online modules of python
+
+# day2
+  -  variables
+  -  rules of writing variables
+  -  data types
+  -  type casting
+  - operators
+  -  taking input
+  -  Strings
+  -  slicing
+  -  string functions
+  -  escape sequance
+  -  f string
