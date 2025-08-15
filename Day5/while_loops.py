f5510d167f334c782cbea8034cc730d34a4bfddd
Unknownityyy <pprem1644@gmail.com>
2025-08-15 16:30:11 +0000

    while loop

---
+++ Day5/while_loops.py
@@ -0,0 +1,13 @@
+i = 1
+
+while(i<51):
+    print(i)
+    i += 1
+
+
+items = ["helo", "ho", "hi", "hey", "hola", True, 22, 3.14, 0, None]
+
+i = 0
+while(i < len(items)):
+    print(items[i])
+    i += 1
