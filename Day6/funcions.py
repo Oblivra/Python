89b522138ac7586b0c84392f61921b90e9b4152d
Unknownityyy <pprem1644@gmail.com>
2025-08-16 13:01:44 +0000

    function

---
+++ Day6/funcions.py
@@ -0,0 +1,33 @@
+# def greet():
+#     print("Hello, welcome to the function practice!")                                   
+
+# greet()
+
+
+
+# def goodMorning(name):
+#     print(f"Good morning {name}!")
+
+# goodMorning("Yorichi")
+
+
+
+
+# def greet(name, ending):
+#     print(f"Hello {name}")
+#     print(f"{ending}")
+
+# a = greet("Yorichi", "Thanks!")
+# print(a)
+
+
+
+
+
+def greet(name, ending):
+    print(f"Hello {name}")
+    print(f"{ending}")
+    return 'ok'
+
+a = greet("Yorichi", "Thanks!")
+print(a)
